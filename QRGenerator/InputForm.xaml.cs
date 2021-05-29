da18a0ff450ba138c82b8b0b194ca4291d45f3fc
Shorotshishir <tanzim121@gmail.com>
2021-05-29 17:03:03 +0900

    Add Form and colored QR

---
+++ QRGenerator/InputForm.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QRGenerator
+{
+    /// <summary>
+    /// Interaction logic for InputForm.xaml
+    /// </summary>
+    public partial class InputForm : UserControl
+    {
+        public InputForm()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+    }
+}
--- QRGenerator/MainWindow.xaml.cs
+++ QRGenerator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using Newtonsoft.Json;
 
 namespace QRGenerator
 {
@@ -14,10 +15,24 @@ namespace QRGenerator
 
         private void GenerateQrButton_Click(object sender, RoutedEventArgs e)
         {
-            var str = TextInput.Text;
-            var bitmap = GenerateQr(str);
+            bool makeRed = false;
+
+            if (IsRed.IsChecked == null || IsRed.IsChecked == false)
+            {
+                makeRed = false;
+            }
+            else
+            {
+                makeRed = true;
+            }
+
+            //var itemData = new ItemData(Id.Text, Ip.Text);
+            var itemData = new ItemData();
+            var data = JsonConvert.SerializeObject(itemData, Formatting.Indented);
+            using var bitmap = GenerateQr(data, makeRed);
             var bitmapImage = BmpImageFromBmp(bitmap);
             ShowImage(bitmapImage);
+            ViewItemData.Content = data;
         }
 
         private void ShowImage(BitmapImage bitmapImage)
@@ -25,31 +40,41 @@ namespace QRGenerator
             Qr.Source = bitmapImage;
         }
 
-        private static Bitmap GenerateQr(string str)
+        private static Bitmap GenerateQr(string str, bool isRed)
         {
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData data = qrGenerator.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(data);
-            Bitmap qrBitmap = qrCode.GetGraphic(20);
-            return qrBitmap;
+
+            //return qrCode.GetGraphic(20, Color.Red, Color.White, true);
+
+            if (isRed)
+            {
+                System.Console.WriteLine("RED");
+                return qrCode.GetGraphic(20, Color.Red, Color.White, true);
+            }
+            else
+            {
+                System.Console.WriteLine("BW");
+
+                return qrCode.GetGraphic(20);
+            }
         }
 
         private static BitmapImage BmpImageFromBmp(Bitmap bmp)
         {
-            using (var memory = new System.IO.MemoryStream())
-            {
-                bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
-                memory.Position = 0;
+            using var memory = new System.IO.MemoryStream();
+            bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+            memory.Position = 0;
 
-                var bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze();
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memory;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
 
-                return bitmapImage;
-            }
+            return bitmapImage;
         }
     }
 }
